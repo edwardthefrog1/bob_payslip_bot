08e15683708a469f9e49c853d86eac19b6068581
frogginbullfish <>
2022-06-04 18:59:12 +1000

    bot can now implicitly wait, and close browser, issues with logout; need to find actual xtags for logout button path

--- bob_bot.py
+++ bob_bot.py
@@ -6,6 +6,8 @@ from main import CONFIG_PATH
 from configparser import ConfigParser
 from ast import literal_eval as litev
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from time import sleep
 
 DRIVER_SAFARI = webdriver.Safari()
 
@@ -25,10 +27,13 @@ def selenium_do_instructions(instructions_config_id, person_id):
     instr_dict = literal_eval_config(instructions_config_id)
     person_id_dict = literal_eval_config(person_id)
     for i, key in enumerate(instr_dict.keys()):
+        DRIVER_SAFARI.implicitly_wait(10)
         if instr_dict[key][-1] == 'send_keys':
+            print('sending key: {}'.format(instr_dict[key][0]))
             DRIVER_SAFARI.find_element_by_xpath(instr_dict[key][0])\
                 .send_keys(list(person_id_dict.values())[i])
         if instr_dict[key][-1] == 'click':
+            print('click: {}'.format(instr_dict[key][0]))
             DRIVER_SAFARI.find_element_by_xpath(instr_dict[key][0])\
                 .click()
 
@@ -40,11 +45,19 @@ class bob_bot:
     def login(self):
         DRIVER_SAFARI.get(literal_eval_config(self.person_id)['url'])
         selenium_do_instructions('login_instructions', self.person_id)
+
+    def logout(self):
+        selenium_do_instructions('logout_instructions', self.person_id)
+
+    def close_safari(self):
+        DRIVER_SAFARI.quit()
         
 
 def main():
     bob_bot_tom = bob_bot('tom')
     bob_bot_tom.login()
+    #bob_bot_tom.logout()
+    bob_bot_tom.close_safari()
 
 
 if __name__ == "__main__":
