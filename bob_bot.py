acad0ab14e7ca10b0bbb37951e353e10bce5a110
frogginbullfish <>
2022-06-04 15:40:17 +1000

    made changes to bob class put literal evaluation code in definition as its used more than once bob classes initialisation was simplified with the lit eval definition

--- bob_bot.py
+++ bob_bot.py
@@ -4,26 +4,32 @@ from selenium import webdriver
 from main import CONFIG_PATH
 from configparser import ConfigParser
 from ast import literal_eval as litev
+from selenium.webdriver.common.keys import Keys
 
 DRIVER_SAFARI = webdriver.Safari()
 
 
-class bob_bot:
-    def __init__(self, person_id):
-        self.person_id = person_id
-        data_tmp, key_tmp = ([] for i in range(2))
-        print('Loading data from config')
-        config = ConfigParser()
-        config.read(CONFIG_PATH)
-        config = dict(config.items(self.person_id))
-        for key in config:
+def literal_eval_config(config_id):
+    data_tmp, key_tmp = ([] for i in range(2))
+    config = ConfigParser()
+    config.read(CONFIG_PATH)
+    config = dict(config.items(config_id))
+    for key in config:
             data_tmp.append(litev(config[key]))
             key_tmp.append(key)
-        self.data_dict = {key_tmp[i]: data_tmp[i] for i in range(len(key_tmp))}
-        print(self.data_dict)
-        
+    return {key_tmp[i]: data_tmp[i] for i in range(len(key_tmp))}
+
+
+class bob_bot:
+    def __init__(self, person_id, xpaths_id='xero_x_paths'):
+        self.person_id = person_id
+        self.x_paths_id = xpaths_id
+        self.login_details_dict = literal_eval_config(self.person_id)
+        self.x_paths_dict = literal_eval_config(self.xpaths_id)
+
     def login(self):
         DRIVER_SAFARI.get(self.data_dict['url'])
+        
 
     
 
@@ -32,8 +38,7 @@ class bob_bot:
 
 def main():
     bob_bot_tom = bob_bot('tom')
-    bob_bot_tom.login()
-
+    #bob_bot_tom.login()
 
 
 if __name__ == "__main__":
