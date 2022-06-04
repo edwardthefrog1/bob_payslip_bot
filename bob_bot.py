97881dda2043283a5bad89b7d6a2bc2402883767
frogginbullfish <>
2022-06-04 14:07:39 +1000

    created bob bot class, and read in config data, not ready yet needs work

--- bob_bot.py
+++ bob_bot.py
@@ -5,24 +5,38 @@ from main import CONFIG_PATH
 from configparser import ConfigParser
 from ast import literal_eval as litev
 
+DRIVER_SAFARI = webdriver.Safari()
 
-def setup_up_selenium(person_id):
-    data_tmp, key_tmp = ([] for i in range(2))
-    dr = webdriver.Safari()
-    config = ConfigParser()
-    config.read(CONFIG_PATH)
-    config = dict(config.items(person_id))
-    for key in config:
-        data_tmp.append(litev(config[key]))
-        key_tmp.append(key)
-    dict_config_data = {key_tmp[i]: data_tmp[i] for i in range(len(key_tmp))}
-    dr.get(dict_config_data[person_id]['url'])
+
+class bob_bot:
+    def __init__(self, person_id):
+        self.person_id = person_id
+        self.data_dict
+
+    def get_data_dict(self):
+        data_tmp, key_tmp = ([] for i in range(2))
+        config = ConfigParser()
+        config.read(CONFIG_PATH)
+        config = dict(config.items(self.person_id))
+        for key in config:
+            data_tmp.append(litev(config[key]))
+            key_tmp.append(key)
+        self.data_dict = {key_tmp[i]: data_tmp[i] for i in range(len(key_tmp))}
+
+    def login(self):
+        DRIVER_SAFARI.get(self.data_dict['url'])
+
+    
+
+
 
 
 def main():
-    setup_up_selenium('tom_xero')
-    while True:
-        a += 1
+    bob_bot_tom = bob_bot('tom_xero')
+    bob_bot_tom.get_data_dict()
+    bob_bot_tom.login()
+
+
 
 if __name__ == "__main__":
     main()
