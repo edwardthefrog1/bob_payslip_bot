b613e502fd0ce2b11e40eac4f772d891551ccd21
frogginbullfish <>
2022-06-11 03:25:44 +1000

    setup questions at start of program finalised, able to get auth code from script without running everything. bot can log in, function for logging in multiple bots added, need to add a list of safari web drivers; then create bots attaching to their own unique safari web driver element in the safari web driver list.

--- bob_bot.py
+++ bob_bot.py
@@ -1,27 +1,54 @@
-from lib2to3.pgen2 import driver
-from re import X
-from tkinter import N
-from pip import main
 from selenium import webdriver
 from main import CONFIG_PATH
 from configparser import ConfigParser
 from ast import literal_eval as litev
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
-from time import sleep
 from authenticator import get_auth_code
 
-print('Start bob bot with webdriver ? (y/n)')
-while(True):    
-    reply = input()
-    if reply == 'y' or reply == 'Y':
-        DRIVER_SAFARI = webdriver.Safari()
-        break
-    elif reply == 'n' or reply == 'N':
-        break
-    else:
-        print('invalid input, please answer with (y/n)')
+
+def setup_run():
+    users_dict = literal_eval_config('auth_code_recipients')
+    print('Want auth code? (y/n):', end='')
+    while(True):    
+        reply = input()
+        if reply == 'y' or reply == 'Y':
+            print('\nSelect auth code recipient:{}'.format(users_dict.keys()))
+            print('I.e: type reply as: ["recipient", "recipient2"]')
+            print('Type users here: ', end='')
+            try:
+                user_s_list = litev(input())
+                for i, user_s in enumerate(user_s_list):
+                    print('auth code: {}'.format(get_auth_code(literal_eval_config(user_s)\
+                    ['secret_key'])))
+            except TypeError:
+                print('could not enumerate list.. please restart and input list')
+                quit()
+            break
+        elif reply == 'n' or reply == 'N':
+            break
+        else:
+            print('invalid input, please answer with (y/n)')
+    print('Start bob bot with webdriver ? (y/n):', end='')
+    while(True):    
+        reply = input()
+        if reply == 'y' or reply == 'Y':
+            driver_safari = webdriver.Safari()
+            break
+        elif reply == 'n' or reply == 'N':
+            break
+        else:
+            print('invalid input, please answer with (y/n)')
+    print('quit or continue from here? (y/n):', end='')
+    while(True):    
+        reply = input()
+        if reply == 'y' or reply == 'Y':
+            break
+        elif reply == 'n' or reply == 'N':
+            quit()
+        else:
+            print('invalid input, please answer with (y/n)')
+    return user_s_list, driver_safari
+
 
 def literal_eval_config(config_id):
     data_tmp, key_tmp = ([] for i in range(2))
@@ -33,6 +60,7 @@ def literal_eval_config(config_id):
             key_tmp.append(key)
     return {key_tmp[i]: data_tmp[i] for i in range(len(key_tmp))}
 
+
 def write_to_config(key, param, data):
     config = ConfigParser()
     config.read(CONFIG_PATH)
@@ -40,63 +68,73 @@ def write_to_config(key, param, data):
     with open(CONFIG_PATH, 'w') as configfile:
         config.write(configfile)
 
-def selenium_do_instructions(instructions_config_id, data_id):
-    sleep(5)
-    try:
-        DRIVER_SAFARI.switch_to().defaultContent()
-    except:
-        print('could not find default frame')
+
+def selenium_do_instructions(instructions_config_id, data_id, driver_safari):
     instr_dict = literal_eval_config(instructions_config_id)
     data_id_dict = literal_eval_config(data_id)
     for i, key in enumerate(instr_dict.keys()):
-
         if instr_dict[key][-1] == 'XPATH':
             if instr_dict[key][1] == 'send_keys':
                 print('sending key: {}'.format(instr_dict[key][0]))
-                DRIVER_SAFARI.find_element(By.XPATH, \
+                driver_safari.find_element(By.XPATH, \
                     instr_dict[key][0]).send_keys(list(data_id_dict.values())[i])
             if instr_dict[key][1] == 'click':
                 print('click: {}'.format(instr_dict[key][0]))
-                DRIVER_SAFARI.find_element(By.XPATH, \
+                driver_safari.find_element(By.XPATH, \
                     instr_dict[key][0]).click()
-
         if instr_dict[key][-1] == 'CLASS_NAME':
             if instr_dict[key][1] == 'send_keys':
                 print('sending key: {}'.format(instr_dict[key][0]))
-                DRIVER_SAFARI.find_element(By.CLASS_NAME, \
+                driver_safari.find_element(By.CLASS_NAME, \
                     instr_dict[key][0]).send_keys(list(data_id_dict.values())[i])
             if instr_dict[key][1] == 'click':
                 print('click: {}'.format(instr_dict[key][0]))
-                DRIVER_SAFARI.find_element(By.CLASS_NAME, \
+                driver_safari.find_element(By.CLASS_NAME, \
                     instr_dict[key][0]).click()
+
+def create_bobs(users, driver_safari):
+    bob_bot_list = []
+    for i, user_s in enumerate(users):
+        bob_bot_list.append(bob_bot(user_s, driver_safari))
+    return bob_bot_list
+
+def login_bob_bots(bob_bot_list):
+    for i, user_s in enumerate(bob_bot_list):
+        print('Logging in {} ...'.format(bob_bot_list[i]))
+        user_s.login()
+        user_s.authenticate_bob()
 
 
 class bob_bot:
-    def __init__(self, person_id):
+    def __init__(self, person_id, driver_safari):
         self.person_id = person_id
+        self.driver_safari = driver_safari
 
     def login(self):
-        DRIVER_SAFARI.get(literal_eval_config(self.person_id)['url'])
-        selenium_do_instructions('login_instructions', self.person_id)
+        self.driver_safari.get(literal_eval_config(self.person_id)['url'])
+        selenium_do_instructions('login_instructions', self.person_id, \
+            self.driver_safari)
 
     def logout(self):
-        selenium_do_instructions('logout_instructions', self.person_id)
+        selenium_do_instructions('logout_instructions', self.person_id, \
+            self.driver_safari)
 
     def close_safari(self):
-        DRIVER_SAFARI.quit()
+        self.driver_safari.quit()
 
     def authenticate_bob(self):
         auth_code = get_auth_code(literal_eval_config(self.person_id)\
             ['secret_key'])
         write_to_config('auth_data', 'auth_code', auth_code)
-        selenium_do_instructions('authenticate_instructions', 'auth_data')
-        
+        selenium_do_instructions('authenticate_instructions', 'auth_data', \
+            self.driver_safari)
+
+
 
 def main():
-    bob_bot_tom = bob_bot('tom')
-    bob_bot_tom.login()
-    bob_bot_tom.authenticate_bob()
-    #bob_bot_tom.logout()
+    users, driver_safari = setup_run()
+    bob_bot_list =  create_bobs(users, driver_safari)
+    login_bob_bots(bob_bot_list)
     #bob_bot_tom.close_safari()
 
 
