f4ae098d6ee548a870ba9df3d08c264acb42c7a4
frogginbullfish <>
2022-06-12 04:04:36 +1000

    made changes to auth for leading zeros, needs to be tested selenium do function has been simplified currently xpath needs work to get phone numbers

--- authenticator.py
+++ authenticator.py
@@ -5,7 +5,7 @@ import time
 def get_auth_code(secret_key):
     totp = TOTP(secret_key)
     while(True):
-        if totp.verify(totp.now()):
-            return totp.now()
+        if totp.verify(totp.now()) and (totp.now()):
+            return str(totp.now())
         else:
-            time.sleep(25)
+            time.sleep(5)
--- bob_bot.py
+++ bob_bot.py
@@ -75,22 +75,36 @@ def write_to_config(key, param, data):
 
 
 def selenium_do_instructions(instructions_config_id, data_id, driver_safari):
+    scraped_data = []
     instr_dict = literal_eval_config(instructions_config_id)
     if data_id != None:
         data_id_dict = literal_eval_config(data_id)
-    for i, key in enumerate(instr_dict.keys()):
-        element = WebDriverWait(driver_safari, 10).until\
-            (EC.presence_of_element_located((By.XPATH, instr_dict[key][0])))
-        print(element)
+    for i, key in enumerate(instr_dict.keys()):    
         if instr_dict[key][-1] == 'XPATH':
-            if instr_dict[key][1] == 'send_keys':
-                print('sending key: {}'.format(instr_dict[key][0]))
-                driver_safari.find_element(By.XPATH, \
-                    instr_dict[key][0]).send_keys(list(data_id_dict.values())[i])
-            if instr_dict[key][1] == 'click':
-                print('click: {}'.format(instr_dict[key][0]))
-                driver_safari.find_element(By.XPATH, \
-                    instr_dict[key][0]).click()
+            by_format = By.XPATH
+        if instr_dict[key][-1] == 'TAG_NAME':
+            by_format = By.TAG_NAME
+        if instr_dict[key][-1] == 'CLASS_NAME':
+            by_format = By.CLASS_NAME  
+        element = WebDriverWait(driver_safari, 30).until\
+            (EC.presence_of_element_located((by_format, instr_dict[key][0])))
+        print(element)
+        if instr_dict[key][1] == 'send_keys':
+            print('sending key: {}'.format(instr_dict[key][0]))
+            driver_safari.find_element(by_format, \
+                instr_dict[key][0]).send_keys(list(data_id_dict.values())[i])
+        if instr_dict[key][1] == 'click':
+            print('click: {}'.format(instr_dict[key][0]))
+            driver_safari.find_element(by_format, \
+                instr_dict[key][0]).click()
+        if instr_dict[key][1] == 'scrape':
+            print('scrape: {}'.format(instr_dict[key][0]))
+            scrape_list = driver_safari.find_elements(by_format,\
+                instr_dict[key][0])
+            for j, scrape in enumerate(scrape_list):
+                scraped_data.append(scrape.text)
+                print(scraped_data[j])
+                
 
 def create_bobs(users, create_driver):
     bob_bot_list = []
@@ -133,12 +147,10 @@ class bob_bot:
         write_to_config('auth_data', 'auth_code', auth_code)
         selenium_do_instructions('authenticate_instructions', 'auth_data', \
             self.driver_safari)
-        print('we got here')
     
     def get_profile_data(self):
         selenium_do_instructions('get_profile_data_instructions', None,\
              self.driver_safari)
-        pass
     
     def get_payslips_data(self):
         pass
@@ -155,6 +167,6 @@ def main():
     user_s_list, create_driver = setup_run()
     bob_bot_list = login_bob_bots(create_bobs(user_s_list, create_driver))
     bob_bot_list[0].get_profile_data()
-
+    sleep(30)
 if __name__ == "__main__":
     main()
