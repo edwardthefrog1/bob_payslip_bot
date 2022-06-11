de4a66e2abc38a66c023905b3d3657ac7f8addf5
frogginbullfish <>
2022-06-11 17:16:05 +1000

    added line to make a quick backup copy of local config

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+import shutil
 CONFIG_PATH = 'config.ini'
+
+
+
+shutil.copy('/Users/frogginbullfish/Documents/python_scripts/bob_bot/config.ini',\
+     '/Users/frogginbullfish/Documents/python_scripts/misc/config_backup.ini')
